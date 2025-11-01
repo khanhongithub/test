34abf8628a76b53c670bc5604eba82c63bc6bc94
khanhongithub <khanh29.nguyendinh@gmail.com>
2025-11-01 17:13:44 +0700

    initial commit

---
+++ static/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+public:
+    // static data member
+    static int x;
+    A(){ cout << "A's constructor is called \n"; }
+};
+
+int A::x = 2;
+
+int main()
+{
+    cout << "Acessing static member \n" << endl;
+    cout << A::x << endl; 
+
+    return 0;
+}
